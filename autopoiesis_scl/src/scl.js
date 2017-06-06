8ce28b66d033194e01bcf4b096bb03f4e8a3334e
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2017-06-07 01:14:02 +0900

    SCL: implement production reaction.

--- autopoiesis_scl/src/scl.js
+++ autopoiesis_scl/src/scl.js
@@ -14,46 +14,65 @@ export default class SCL extends DynamicalSystem {
         this.cells = new Array(this.x_size);
         for(let x = 0; x < this.x_size; x++) {
             this.cells[x] = new Array(this.y_size);
-            for (var y = 0; y < this.y_size; y++) {
+            for (let y = 0; y < this.y_size; y++) {
                 if ( utils.eval_prob(0.8) ) {
                 //if ( utils.eval_prob(0.8) &&  i > 10 && i<20 && j>20 && j<30) {
-                    this.cells[x][y] = {'type':'S', 'bonds':[], 'mobile':true};
+                    this.cells[x][y] = {
+                        'type': 'S',
+                        'bonds': [],
+                        'mobile': true,
+                        'mobility_factor': 0.1
+                    };
                 } else {
-                    this.cells[x][y] = {'type':'H', 'bonds':[], 'mobile':true};
+                    this.cells[x][y] = {
+                        'type': 'H',
+                        'bonds': [],
+                        'mobile': true,
+                        'mobility_factor': 0.1
+                    };
                 }
             }
         }
         this.cells[this.x_size/2][this.y_size/2] = {'type':'C', 'bonds':[], 'mobile':true};
+
+        this.mobility_factors = {
+            'H':  0.1,
+            'S':  0.1,
+            'C':  0.001,
+            'L':  0.1,
+            'LS': 0.1,
+        }
+        this.production_prob = 0.1;
     }
 
     update() {
         for(let x = 0; x < this.x_size; x++) {
-            for (var y = 0; y < this.y_size; y++) {
-                SCL.motion(this.cells, x, y);
+            for (let y = 0; y < this.y_size; y++) {
+                SCL.motion(this.cells, x, y, this.mobility_factors);
             }
         }
         SCL.reset_mobile(this.cells);
 
         for(let x = 0; x < this.x_size; x++) {
-            for (var y = 0; y < this.y_size; y++) {
-                SCL.production(this.cells, x, y)
-                SCL.disintegration(this.cells, x, y)
-                SCL.bonding(this.cells, x, y)
-                SCL.bond_decay(this.cells, x, y)
-                SCL.absorption(this.cells, x, y)
-                SCL.emission(this.cells, x, y)
+            for (let y = 0; y < this.y_size; y++) {
+                SCL.production(this.cells, x, y, this.production_prob);
+                //SCL.disintegration(this.cells, x, y);
+                //SCL.bonding(this.cells, x, y);
+                //SCL.bond_decay(this.cells, x, y);
+                //SCL.absorption(this.cells, x, y);
+                //SCL.emission(this.cells, x, y);
             }
         }
 
         super.update_state();
     }
 
-    static motion(cells, x, y) {
+    static motion(cells, x, y, mobility_factors) {
         let p = cells[x][y];
         let [np, nx, ny] = utils.get_rand_neumann_neighborhood(cells, x, y);
         if (p.mobile && np.mobile && p.type!=np.type && p.bonds.length==0 && np.bonds.length==0) {
-            let mobility_factor = 0.1;
-            if (utils.eval_prob(mobility_factor)) {
+            let prob = Math.sqrt(mobility_factors[p.type] * mobility_factors[np.type]);
+            if (utils.eval_prob(prob)) {
                 cells[x][y] = np;
                 cells[nx][ny] = p;
                 cells[x][y].mobile = false;
@@ -64,13 +83,27 @@ export default class SCL extends DynamicalSystem {
 
     static reset_mobile(cells) {
         for(let x = 0; x < cells.length; x++) {
-            for (var y = 0; y < cells[x].length; y++) {
+            for (let y = 0; y < cells[x].length; y++) {
                 cells[x][y].mobile = true;
             }
         }
     }
 
-    static production(cells, x, y) {}
+    static production(cells, x, y, production_prob) {
+        let p = cells[x][y];
+        if (p.type != 'C') {
+            return
+        }
+        let [np1, nx1, ny1, np2, nx2, ny2] = utils.get_rand_2_moore_neighborhood(cells, x, y);
+        if (np1.type != 'S' || np2.type != 'S') {
+            return
+        }
+        if (utils.eval_prob(production_prob)) {
+            np1.type = 'H';
+            np2.type = 'L'
+        }
+    }
+
     static disintegration(cells, x, y) {}
     static bonding(cells, x, y) {}
     static bond_decay(cells, x, y) {}
--- autopoiesis_scl/src/utils.js
+++ autopoiesis_scl/src/utils.js
@@ -6,10 +6,13 @@ const utils = {
     get_rand_neumann_neighborhood: (cells, x, y) => {
         let x_size = cells.length;
         let y_size = cells[x].length;
-        let xy = parseInt(Math.random() * 2);
-        let i = parseInt(Math.random() * 2) * 2 - 1
-        let nx = (x +  xy * i + x_size) % x_size;
-        let ny = (y + !xy * i + y_size) % y_size;
+        let neighborhood = [
+            [ x                 , (y-1+y_size)%y_size],
+            [(x-1+x_size)%x_size, y],
+            [(x+1)%x_size       , y],
+            [ x                 , (y+1)%y_size]
+        ];
+        let [nx,ny] = neighborhood[Math.floor(Math.random()*neighborhood.length)];
         let np = cells[nx][ny]
         return [np, nx, ny]
     },
@@ -17,10 +20,45 @@ const utils = {
     get_rand_moor_neighborhood: (cells, x, y) => {
         let x_size = cells.length;
         let y_size = cells[x].length;
-        let nx = (x + parseInt(Math.random() * 2) * 2 - 1 + x_size) % x_size;
-        let ny = (y + parseInt(Math.random() * 2) * 2 - 1 + y_size) % y_size;
+        let neighborhood = [
+            [(x-1+x_size)%x_size, (y-1+y_size)%y_size],
+            [ x                 , (y-1+y_size)%y_size],
+            [(x+1)%x_size       , (y-1+y_size)%y_size],
+            [(x-1+x_size)%x_size, y],
+            [(x+1)%x_size       , y],
+            [(x-1+x_size)%x_size, (y+1)%y_size],
+            [ x                 , (y+1)%y_size],
+            [(x+1)%x_size       , (y+1)%y_size]
+        ];
+        let [nx,ny] = neighborhood[Math.floor(Math.random()*neighborhood.length)];
         let np = cells[nx][ny]
         return [np, nx, ny]
+    },
+
+    get_rand_2_moore_neighborhood: (cells, x, y) => {
+        let x_size = cells.length;
+        let y_size = cells[x].length;
+        let [np1, nx1, ny1] = utils.get_rand_moor_neighborhood(cells, x, y);
+        let neighborhood2;
+        if (x == nx1) {
+            neighborhood2 = [
+                [(nx1-1+x_size)%x_size, ny1],
+                [(nx1+1)%x_size       , ny1]
+            ]
+        } else if (y == ny1) {
+            neighborhood2 = [
+                [nx1, (ny1-1+y_size)%y_size],
+                [nx1, (ny1+1)%y_size]
+            ]
+        } else {
+            neighborhood2 = [
+                [nx1, y],
+                [x, ny1]
+            ]
+        }
+        let [nx2,ny2] = neighborhood2[Math.floor(Math.random()*neighborhood2.length)];
+        let np2 = cells[nx2][ny2];
+        return [np1, nx1, ny1, np2, nx2, ny2]
     }
 }
 
